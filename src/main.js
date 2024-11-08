312554e9577ea12d1d4eb00684aded02e556924c
Adam Marko <adko87@hotmail.com>
2024-11-08 11:30:03 -0700

    aaa

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import DocumentUploader from './components/DocumentUploader.svelte';
 
 const app = new DocumentUploader({
-    target: document.body,
+    // @ts-ignore
+    target: document.getElementById('app'),
     props: {},
 });
 
