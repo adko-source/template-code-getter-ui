428eaaaeaf068605eb93bda0a4d2b3d04e183277
Adam <61096551+adko-source@users.noreply.github.com>
2024-11-07 19:32:27 -0700

    Add files via upload

---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+
+    import DocumentUploader from '../components/DocumentUploader.svelte';
+	
+  </script>
+  
+  <svelte:head>
+	<title>Template Code Generator</title>
+	<meta name="description" content="Template Code Generator" />
+</svelte:head>
+  
+ 
+<DocumentUploader />
